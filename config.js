47bff8f9c9a28be1617fd8ba0be39366443a18d0
batideco <126428054+batideco@users.noreply.github.com>
2023-02-27 01:00:22 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports={
+user:'batideco.fr@gmail.com',
+clientId:'967452195136-dittgpfidkamkjm4ijnicqsjceh6u153.apps.googleusercontent.com',
+clientSecret:'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',
+refreshToken:'1//045d-r83FlMyQCgYIARAAGAQSNwF-L9IrPh9SIKey0xmrLCXhFWvtGvElAdy3qfUDdDlIHwe8nogGYKaHqwANPqpX02ehW92LSAg'
+}
